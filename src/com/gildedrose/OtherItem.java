e892198b6de029a76846ee9ae6c9f637dd27c292
Brian Gibson <bggibson@hotmail.com>
2017-01-13 11:35:12 +0100

    Refactor Item subclasses

--- src/com/gildedrose/OtherItem.java
+++ src/com/gildedrose/OtherItem.java
@@ -7,51 +7,14 @@ class OtherItem extends Item {
 
     @Override
     void UpdateQuality() {
-        if (!name.equals(AGED_BRIE)
-                && !name.equals(Item.BACKSTAGE_PASS)) {
-            if (quality > 0) {
-                if (!name.equals(Item.SULFURAS)) {
-                    quality = quality - 1;
-                }
-            }
-        } else if (quality < 50) {
-            quality = quality + 1;
-
-            if (name.equals(Item.BACKSTAGE_PASS)) {
-                if (sellIn < 11) {
-                    if (quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-
-                if (sellIn < 6) {
-                    if (quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-            }
+        if (quality > 0) {
+            quality = quality - 1;
         }
 
-        if (!name.equals(Item.SULFURAS)) {
-            sellIn = sellIn - 1;
-        }
+        sellIn = sellIn - 1;
 
         if (sellIn < 0) {
-            if (!name.equals(Item.AGED_BRIE)) {
-                if (!name.equals(Item.BACKSTAGE_PASS)) {
-                    if (quality > 0) {
-                        if (!name.equals(Item.SULFURAS)) {
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    quality = 0;
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
+            quality = quality - 1;
         }
     }
 }
--- src/com/gildedrose/SulfurasItem.java
+++ src/com/gildedrose/SulfurasItem.java
@@ -7,51 +7,5 @@ class SulfurasItem extends Item {
 
     @Override
     void UpdateQuality() {
-        if (!name.equals(AGED_BRIE)
-                && !name.equals(Item.BACKSTAGE_PASS)) {
-            if (quality > 0) {
-                if (!name.equals(Item.SULFURAS)) {
-                    quality = quality - 1;
-                }
-            }
-        } else if (quality < 50) {
-            quality = quality + 1;
-
-            if (name.equals(Item.BACKSTAGE_PASS)) {
-                if (sellIn < 11) {
-                    if (quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-
-                if (sellIn < 6) {
-                    if (quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-            }
-        }
-
-        if (!name.equals(Item.SULFURAS)) {
-            sellIn = sellIn - 1;
-        }
-
-        if (sellIn < 0) {
-            if (!name.equals(Item.AGED_BRIE)) {
-                if (!name.equals(Item.BACKSTAGE_PASS)) {
-                    if (quality > 0) {
-                        if (!name.equals(Item.SULFURAS)) {
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    quality = 0;
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
-        }
     }
 }
